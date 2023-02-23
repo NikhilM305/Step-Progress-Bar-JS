6e8bb1e027eb9293502f8c8d1f3f0785e722998e
NikhilM305 <nikhil.ninja007@gmail.com>
2023-02-23 23:50:31 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,77 @@
+const nextEl = document.getElementById("next");
+const prevEl = document.getElementById("prev");
+const progressEl=document.querySelector(".progress-bar-front")
+const stepsEl = document.querySelectorAll(".step");
+
+let currentChecked = 1;
+
+nextEl.addEventListener("click",()=>{
+   currentChecked++;
+
+if(currentChecked>stepsEl.length){
+    currentChecked=stepsEl.length;
+}
+
+updateStepProgress();
+
+})
+ 
+
+
+prevEl.addEventListener("click",()=>{
+   currentChecked--;
+
+if(currentChecked<1){
+    currentChecked=1;
+}
+
+updateStepProgress();
+
+})
+
+
+
+function updateStepProgress(){
+    stepsEl.forEach((stepEl,index) => {
+    
+        if (index < currentChecked) {
+          stepEl.classList.add("checked");
+
+          stepEl.innerHTML=`
+
+          <i class="fa-solid fa-check"></i>
+           <small>${index===0 ? "Start" : index === stepsEl.length-1 ? "Final" : "Step "+index} </small>
+          
+          `;
+          
+     }
+     else{
+                 stepEl.classList.remove("checked")
+             
+                 stepEl.innerHTML = `
+                 <i class="fa-solid fa-xmark"></i>
+                 `;
+
+     }
+        });
+
+   
+        const checkedNumber = document.querySelectorAll(".checked")
+
+        
+progressEl.style.width = ((checkedNumber.length-1)/(stepsEl.length-1))*100+"%"
+
+
+if(currentChecked === 1){
+    prevEl.disabled = true;
+}
+else if(currentChecked === stepsEl.length){
+    nextEl.disabled = true;
+}
+else{
+    prevEl.disabled = false;
+    nextEl.disabled = false;
+}
+}
+
+
